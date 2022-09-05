6c08d8d573b87e3c6c3d89c0b414c3119d860f2d
Carsten Koch <carsten.b.koch@gmail.com>
2022-09-05 20:51:05 +0200

    fix: not using the props of the Construct yet

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { RemovalPolicy } from 'aws-cdk-lib';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
-export interface QuickSightTestConstructProps {
+// export interface QuickSightTestConstructProps {
 
-}
+// }
 
 export class QuickSightTestConstruct extends Construct {
   public readonly bucketName: string;
 
-  constructor(scope: Construct, id: string, props?: QuickSightTestConstructProps) {
+  constructor(scope: Construct, id: string) {
     super(scope, id);
 
     const bucket = new Bucket(this, 'QSCTestBucket', {
