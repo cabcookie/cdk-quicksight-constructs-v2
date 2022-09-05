a2be88b82ac87e0949220e88db96f978d7697a40
Carsten Koch <carsten.b.koch@gmail.com>
2022-09-05 16:50:24 +0200

    chore: project created with projen

--- .projenrc.js
+++ .projenrc.js
@@ -2,10 +2,10 @@ const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Carsten Koch',
   authorAddress: 'carsten.b.koch@gmail.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.40.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-quicksight-constructs-v2',
-  repositoryUrl: 'https://github.com/carsten.b.koch/cdk-quicksight-constructs-v2.git',
+  repositoryUrl: 'https://github.com/cabcookie/cdk-quicksight-constructs-v2.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
